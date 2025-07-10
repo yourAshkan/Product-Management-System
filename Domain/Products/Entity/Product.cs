d8c2dca05dd3e9bb7de294f6176ef09e2febd17e
yourAshkan <ashkan.kiya11@gmail.com>
2025-07-10 15:29:06 +0330

    Add project files.

---
+++ Domain/Products/Entity/Product.cs
@@ -0,0 +1,36 @@
+﻿using NadinSoftTask.Domain.Users.Entity;
+
+namespace NadinSoftTask.Domain.Products.Entity;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; private set; }
+    public DateTime ProduceDate { get; private set; }
+    public string ManufactureEmail { get; set; }
+    public string ManufacturePhone { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+
+    public Product(string title, DateTime produceDate, string manufactureEmail, string manufacturePhone, int userId)
+    {
+        Title = title;
+        ProduceDate = produceDate;
+        ManufactureEmail = manufactureEmail;
+        ManufacturePhone = manufacturePhone;
+        UserId = userId;
+    }
+
+
+    public bool CanModify(int currentUserId)
+    {
+       return UserId == currentUserId;
+    }
+    public void MarkAsUnAvailable()
+    {
+        IsAvailable = false;
+    }
+}
+
---
+++ Domain/Users/Entity/User.cs
@@ -0,0 +1,10 @@
+﻿namespace NadinSoftTask.Domain.Users.Entity;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string? EmailAddress { get; private set; }
+}
