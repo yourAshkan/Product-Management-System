3798a7c59b28bf99b89273049caa20623158fe18
yourAshkan <ashkan.kiya11@gmail.com>
2025-07-12 17:35:19 +0330

    TST - 5 - Appliction Delete Command

---
+++ NadinSoftTask.Application/Commands/DeleteProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace NadinSoftTask.Application.Commands;
+
+public class DeleteProductCommand : IRequest<bool>
+{
+    public int ProductId { get; init; }
+    public int CurrentUserId { get; init; }
+}
---
+++ NadinSoftTask.Application/Commands/DeleteProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NadinSoftTask.Application.Interfaces;
+
+namespace NadinSoftTask.Application.Commands;
+
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, bool>
+{
+    private readonly IProductRepository _repo;
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _repo = productRepository;
+    }
+    public async Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var prodcut = await _repo.GetByIdAsync(request.ProductId);
+        if (prodcut == null)
+            throw new Exception("Product not found");
+
+        if (!prodcut.CanModify(request.CurrentUserId))
+            throw new Exception("You are not allowed to modify this product!");
+
+
+        await _repo.DeleteAsync(prodcut);
+        return true;
+    }
+}
