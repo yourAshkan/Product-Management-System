4ce4573a7b71ff8a93c5360c57d7eb8f0aaaf596
yourAshkan <ashkan.kiya11@gmail.com>
2025-07-15 15:13:47 +0330

    TST - 12 - Rename the Domain File

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -7,7 +7,7 @@ public class Product
     public int Id { get; set; }
     public string Title { get; private set; }
     public DateTime ProduceDate { get; private set; }
-    public string ManufactureEmail { get; set; }
+    public string ManufactureEmail { get;  set; }
     public string ManufacturePhone { get; set; }
     public bool IsAvailable { get; set; } = true;
 
