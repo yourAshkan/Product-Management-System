6078e6722ec2579040c87cc3cef113f0e4798c64
yourAshkan <ashkan.kiya11@gmail.com>
2025-07-15 18:29:35 +0330

    TST - 16 - Dependency Injection impilimented in Application Layer

---
+++ NadinSoftTask.Application/Commans/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NadinSoftTask.Application.Commans
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection ApplicationRegister(this IServiceCollection services)
+        {
+            services.AddMediatR(x=>x.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+
+            return services;
+        }
+    }
+}
