d34fbb61f57bc45191678f92fd9236233f435c74
yourAshkan <ashkan.kiya11@gmail.com>
2025-07-16 13:03:26 +0330

    TST - 24 - Setup Identity & JWT Auth with Clean DI

--- NadinSoftTask.WebApi/Program.cs
+++ NadinSoftTask.WebApi/Program.cs
@@ -1,25 +1,12 @@
-using NadinSoftTask.Infrastructure.Bootstrappers;
 using NadinSoftTask.WebApi.Commons;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.ContextRegister(builder.Configuration);
-builder.Services.AddWebApiService();
+builder.Services.AddWebApiService(builder.Configuration);
 
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseWebApiMiddlewares();
 
 app.Run();
