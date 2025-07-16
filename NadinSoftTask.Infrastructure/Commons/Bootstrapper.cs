62c8b11cad9eb8d1f8ce4dfa580b0bbbf26afc48
yourAshkan <ashkan.kiya11@gmail.com>
2025-07-16 12:50:35 +0330

    TST - 21 - Changing JWT Configuration from Infrastructure(Bootstrapper) to WebApi(Bootstrapper)

--- NadinSoftTask.Infrastructure/Commons/Bootstrapper.cs
+++ NadinSoftTask.Infrastructure/Commons/Bootstrapper.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using NadinSoftTask.Application.Interfaces;
 using NadinSoftTask.Infrastructure.DataBaseContext;
-using System.Text;
 
 namespace NadinSoftTask.Infrastructure.Commons;
 
@@ -21,25 +18,6 @@ public static class Bootstrapper
         service.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultTokenProviders();
-
-        service.AddAuthentication(x =>
-        {
-            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(x =>
-        {
-            x.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = configuration["JWT:Issuer"],
-                ValidAudience = configuration["JWT:Audience"],
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(configuration["JWT:Key"]))
-            };
-        });
 
         service.AddScoped<IProductRepository, ProdcutRepository>();
 
