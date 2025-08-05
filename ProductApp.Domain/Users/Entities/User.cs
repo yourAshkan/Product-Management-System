14513f3309d33ccef4075d6c55a277d261f72cd2
yourAshkan <ashkan.kiya11@gmail.com>
2025-08-05 15:07:47 +0330

    TST - 32 - Remove Identity package

--- ProductApp.Domain/Users/Entities/User.cs
+++ ProductApp.Domain/Users/Entities/User.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿namespace ProductApp.Domain.Users.Entities;
 
-namespace ProductApp.Domain.Users.Entities;
-
-public class User : IdentityUser<int>
+public class User
 {
     public User()
     {
     }
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
+    public string PhoneNumber { get; set; }
     public string NationalCode { get; set; }
     public string Email { get; set; }
 }
